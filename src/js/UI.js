83ecf10c916a01cb1c6a8b63c2c7b3908aba6f29
eriika19 <enciso.iq@gmail.com>
2019-11-21 14:53:30 -0600

    se bajaran cambios

--- src/js/UI.js
+++ src/js/UI.js
@@ -1,88 +1,85 @@
 class UI {
-     constructor() {
-          // Instanciar la API
-          this.api = new API();
-          
-          // Crear los mapas en un grupo
-          this.markers = new L.LayerGroup(); 
-          
-          // Iniciar el mapa
-          this.mapa = this.inicializarMapa();
+  constructor() {
+    // Instanciar la API
+    this.api = new API();
 
-     }
+    // Crear los mapas en un grupo
+    this.markers = new L.LayerGroup();
 
-     inicializarMapa() {
-     
-       
-          // Inicializar y obtener la propiedad del mapa
-      
-          const map = L.map('map').setView([19.390519, -99.3739778], 6);
+    // Iniciar el mapa
+    this.map = this.mapInit();
+  }
 
-          const enlaceMapa = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+  mapInit() {
+    // Inicializar y obtener la propiedad del mapa
+    const map = L.map("map").setView([19.390519, -99.3739778], 6);
 
-          L.tileLayer(
-              'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-              attribution: '&copy; ' + enlaceMapa + ' Contributors',
-              maxZoom: 18,
-              }).addTo(map);
+    const mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
 
-          return map;
+    L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "&copy; " + mapLink + " Contributors",
+      maxZoom: 18
+    }).addTo(map);
 
-     }
+    return map;
+  }
 
-     // Mostrar Establecimientos de la api
-     mostrarEstablecimientos() {
-          this.api.obtenerDatos()
-                    .then(datos =>  {
-                         const resultado = datos.respuestaJSON.results;
-                         // Muestra los pines en el Mapa
-                         this.mostrarPines(resultado);
-                    } )
-     }
-     // Muestra los pines
-     mostrarPines(datos) {
+  // Mostrar Establecimientos de la api
+  showStations() {
+    this.api.getData().then(data => {
+      //const result = data;
+      //console.log(data);
 
-          this.markers.clearLayers();
-         
-          // Recorrer establecimientos
-          datos.forEach(dato => {
-               // Destucturing 
-               const {latitude, longitude, calle, regular, premium} = dato;
+      // Muestra los pines en el Mapa
+      this.showPins(data);
+    });
+  }
+  // Muestra los pines
+  showPins(data) {
+    this.markers.clearLayers();
 
-               const opcionesPopUp = L.popup()
-               .setContent(`<p>Calle: ${calle}</p> 
-                            <p></p><b>Regular:</b>$ ${regular}</p>
-                            <p> <b>Premium:</b>$ ${premium}</p>`);
-       
-               // Agregar el Pin
-               const marker = new L.marker([
-                    parseFloat(latitude),
-                    parseFloat(longitude)
-               ] )
-               .bindPopup(opcionesPopUp)
+    // Recorrer establecimientos
+    data.forEach(element => {
+      // Destructuración
+      const { id, name, plug_type, kw_price, state, geolocation } = element;
 
-               this.markers.addLayer(marker); 
-          });
-          this.markers.addTo(this.mapa)
-     }
+      const optionsPopUp = L.popup().setContent(`
+                            <p>Dirección:</p> 
+                            <p> <b>Nombre:</b> ${name}</p>
+                            <p> <b>Precio:</b> $ ${kw_price}</p>
+                            <p> <b>Conector:</b> ${plug_type}</p>
+                            <p> <b>Disponibilidad:</b> ${state}</p>
+                            `);
 
-      // Obtiene las sugerencias de la REST API
-      obtenerSugerencias(busqueda) {
-          this.api.obtenerDatos()
-               .then(datos => {
-                    // Obtener los resultados
-                    const resultados = datos.respuestaJSON.results;
+      // Agregar el Pin
+      const marker = new L.marker([
+        parseFloat(geolocation.latitude),
+        parseFloat(geolocation.longitude)
+      ]).bindPopup(optionsPopUp);
 
-                    // Enviar el JSON y la busqueda al Filtro
-                    this.filtrarSugerencias(resultados, busqueda);
-               })
-     }
+      this.markers.addLayer(marker);
+    });
+    this.markers.addTo(this.map);
+  }
 
-     // Filtrar las sugerencias de busqueda
-      filtrarSugerencias(resultados, busqueda) {
-          const filtro = resultados.filter( filtro => filtro.calle.indexOf(busqueda) !== -1 );
+  // Obtiene las sugerencias de la REST API
+  obtenerSugerencias(search) {
+    this.api.getData().then(data => {
+      // Obtener los resultados
+      //const resultados = datos.respuestaJSON.results;
 
-          // Mostrar pines del Filtro
-          this.mostrarPines(filtro);
-     }
+      // Enviar el JSON y la busqueda al Filtro
+      this.filtrarSugerencias(data, search);
+    });
+  }
+
+  // Filtrar las sugerencias de busqueda
+  filtrarSugerencias(data, search) {
+    const filterData = data.filter(
+      filter => filter.state.indexOf(search) !== -1
+    );
+
+    // Mostrar pines del Filtro
+    this.showPins(filterData);
+  }
 }
